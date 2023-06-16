1ec52d009717f1a5d9065f067f8468a925eec86e
yfeng <yfeng@memoflow.com>
2023-06-16 15:05:54 -0700

    Pipeline init

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,8 +1,27 @@
 import * as cdk from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, "AwesomePipeline", {
+      pipelineName: "AwesomePipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "Iostream3100/aws-cicd",
+          "cicd-practice"
+        ),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ],
+      }),
+    });
   }
 }
