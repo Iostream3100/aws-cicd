e323fc4b6b4d2880c179941e864b1244dcde371e
yfeng <iostream3100@gmail.com>
2023-06-16 17:33:42 -0700

    add test stage and lambda stack

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -5,25 +5,25 @@ import {
   ShellStep,
 } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
+import { PipelineStage } from "./PipelineStage";
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, "AwesomePipeline", {
+    const pipeline = new CodePipeline(this, "AwesomePipeline", {
       pipelineName: "AwesomePipeline",
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub(
-          "Iostream3100/aws-cicd",
-          "master"
-        ),
-        commands: [
-          'npm ci',
-          'npx cdk synth'
-        ],
-        primaryOutputDirectory: 'cdk.out'
-
+        input: CodePipelineSource.gitHub("Iostream3100/aws-cicd", "main"),
+        commands: ["npm ci", "npx cdk synth"],
+        primaryOutputDirectory: "cdk.out",
       }),
     });
+
+    const testStage = pipeline.addStage(
+      new PipelineStage(this, "PipelineTestStage", {
+        stageName: "test",
+      })
+    );
   }
 }
